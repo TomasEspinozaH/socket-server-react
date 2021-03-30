7e4937438447fca962976e65a7e2e7e8eaf3652a
TomasEspinoza98 <tomasceatoleii1998@gmail.com>
2021-03-30 19:30:15 -0300

    update

--- models/server.js
+++ models/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
+const cors = require('cors');
 
 const Sockets = require('./sockets');
 
@@ -23,6 +24,9 @@ class Server {
   middlewares() {
     //desplegar el directorio publico
     this.app.use(express.static(path.resolve(__dirname, '../public')));
+
+    //Cors
+    this.app.use(cors());
   }
 
   configSockets() {
